474ea60c8b29888166203ede0eba4e689984d0a7
SachinBalagam <balagamsachin337@gmail.com>
2024-05-16 15:40:29 +0530

    Test commit

--- business-ui/src/App.tsx
+++ business-ui/src/App.tsx
@@ -6,11 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Get Ready for the first Sprint in developing new project.. </h1>
-  
         <h1>Start Coding</h1>
         <h1>Start Coding please</h1>
         <p>Good Wishes for the Project..</p>
-
+        <p>Creating new line for a pull request...</p>
       </header>
     </div>
   );
