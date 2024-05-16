078904759f5c662f61d2e80a5e4b8a1a174f07e3
shiva prasad m <110959424+shivaprasadmakela@users.noreply.github.com>
2024-05-16 15:18:15 +0530

    added h1 in app.tsx

--- business-ui/src/App.tsx
+++ business-ui/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -7,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Get Ready for the first Sprint in developing new project.. </h1>
+        <h1>Start Coding</h1>
       </header>
     </div>
   );
